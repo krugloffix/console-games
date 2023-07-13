ad98e6b62cb49a1a3fc1083cd6b213caac8bb41b
Irina Kruglova <97954023+krugloffix@users.noreply.github.com>
2023-07-13 16:26:49 +0700

    fix linting

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const getPlayerName = () => {
+const getPlayerName = () => {
   const playerName = readlineSync.question('May I have your name? ');
 
   return playerName;
 };
+
+export default getPlayerName();
