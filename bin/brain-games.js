3055b608d116a2e5c4e519028d5e243c042cab8d
Irina Kruglova <97954023+krugloffix@users.noreply.github.com>
2023-07-13 13:30:29 +0700

    add greetings by name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+
+import { getPlayerName } from "../src/cli.js";
+
 console.log("Welcome to the Brain Games!");
+const playerName = getPlayerName();
+console.log(`Hello, ${playerName}!`);
