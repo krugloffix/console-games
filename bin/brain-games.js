d9d732b918d3dd801720d187c4393e2cf8388734
Irina Kruglova <97954023+krugloffix@users.noreply.github.com>
2023-07-13 16:40:37 +0700

    fix linting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
-import { getPlayerName } from '../src/cli';
+import getPlayerName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const playerName = getPlayerName();
 console.log(`Hello, ${playerName}!`);
+
+export default playerName;
