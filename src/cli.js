aef271f1f9771bbeb63b6b84e253610903345f64
Irina Kruglova <97954023+krugloffix@users.noreply.github.com>
2023-07-13 13:28:49 +0700

    upd func getPlayerName

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,4 @@ import readlineSync from "readline-sync";
 
 export const getPlayerName = () => {
   const playerName = readlineSync.question("May I have your name? ");
-  console.log(`Hello, ${playerName}!`);
 };
