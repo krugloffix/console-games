0f0aa3bebdd0b979d108218db0f0e2ea901f3c33
Irina Kruglova <97954023+krugloffix@users.noreply.github.com>
2023-07-16 21:55:38 +0700

    fix game logic

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -4,7 +4,7 @@ const getGcd = (num1, num2) => {
   let gcd = 1;
   for (let i = Math.min(num1, num2); i > 1; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
-      gcd = i;
+      return gcd = i;
     }
   }
 
